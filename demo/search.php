52e13122e799f8bd40e5ecff8b34704707a71b38
Dylan Verheul <dylan.verheul@gmail.com>
2011-12-21 08:44:24 +0100

    Minor bug fix

--- demo/search.php
+++ demo/search.php
@@ -1,34 +1,51 @@
 <?php
 
+/*
+ * Load sample data
+ */
 include 'data.php';
 
+/*
+ * Results array
+ */
 $results = array();
 
-$q = '';
+/*
+ * Autocomplete formatter
+ */
+function autocomplete_format($results) {
+    foreach ($results as $result) {
+        echo $result[0] . '|' . $result[1] . "\n";
+    }
+}
+
+/*
+ * Search for term if it is given
+ */
 if (isset($_GET['q'])) {
     $q = strtolower($_GET['q']);
+    if ($q) {
+        foreach ($data as $key => $value) {
+            if (strpos(strtolower($key), $q) !== false) {
+                $results[] = array($key, $value);
+            }
+        }
+    }
 }
+
+/*
+ * Output format
+ */
 $output = 'autocomplete';
 if (isset($_GET['output'])) {
     $output = strtolower($_GET['output']);
 }
 
-if ($q) {
-    foreach ($data as $key => $value) {
-        if (strpos(strtolower($key), $q) !== false) {
-            $results[] = array($key, $value);
-        }
-    }
-}
-
+/*
+ * Output results
+ */
 if ($output === 'json') {
     echo json_encode($results);
 } else {
     echo autocomplete_format($results);
-}
-
-function autocomplete_format($results) {
-    foreach ($results as $result) {
-        echo $result[0] . '|' . $result[1] . "\n";
-    }
 }
